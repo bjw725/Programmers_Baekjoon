f153f596e1442ef30ed0ebdb14d5f6e5ad2699ea
Jaewon Bae / 배재원 <135415380+bjw725@users.noreply.github.com>
2023-07-08 00:16:49 +0900

    [level 2] Title: 타겟 넘버, Time: 3.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,15 @@
+def dfs(numbers, target, curr_sum, curr_idx):
+    if curr_idx == len(numbers):
+        if curr_sum == target:
+            return 1
+        else:
+            return 0
+    else:
+        answer = 0
+        answer += dfs(numbers, target, curr_sum + numbers[curr_idx], curr_idx + 1) 
+        answer += dfs(numbers, target, curr_sum - numbers[curr_idx], curr_idx + 1)
+    return answer
+
+def solution(numbers, target):
+    answer = dfs(numbers, target, 0, 0)
+    return answer
